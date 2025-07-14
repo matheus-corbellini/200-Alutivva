eb7f50c4c23771d4b14bfafc1fb695a49562c89c
Matheus Corbellini <matheussteindorff@gmail.com>
2025-07-14 09:40:05 -0300

    inicio do projeto

---
+++ src/firebaseconfig.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC3FlXVw9Kplmq_bb23zO5QznpVWgmxjyU",
+  authDomain: "alutivva.firebaseapp.com",
+  projectId: "alutivva",
+  storageBucket: "alutivva.firebasestorage.app",
+  messagingSenderId: "733990232313",
+  appId: "1:733990232313:web:b7bd0aad53ca7fd7c34228",
+};
+
+const app = initializeApp(firebaseConfig);
