f5555d252a8bcf1e74ed220b3da6bde118210ef5
Matheus Corbellini <matheussteindorff@gmail.com>
2025-07-22 13:24:27 -0300

    Remove arquivo index.ts duplicado

--- src/components/marketplace/index.ts
+++
@@ -1,8 +0,0 @@
-export { Badge } from "./Badge";
-export { EmptyState } from "./EmptyState";
-export { FiltersSection } from "./FiltersSection";
-export { Header } from "./Header";
-export { HeroSection } from "./HeroSection";
-export { PropertiesGrid } from "./PropertiesGrid";
-export { PropertyCard } from "./PropertyCard";
-export { ResultsSummary } from "./ResultsSummary";
