ce9926fa88f9c9c00c86f0ba9aa5a3e8ddf3355e
Matheus Corbellini <matheussteindorff@gmail.com>
2025-07-22 14:33:35 -0300

    remocao de pasta

--- src/components/marketplace/index.tsx
+++
@@ -1,9 +0,0 @@
-// Index file for Marketplace components
-export { Badge } from "./Badge";
-export { Header } from "./Header";
-export { HeroSection } from "./HeroSection";
-export { FiltersSection } from "./FiltersSection";
-export { ResultsSummary } from "./ResultsSummary";
-export { PropertiesGrid } from "./PropertiesGrid";
-export { EmptyState } from "./EmptyState";
-export { PropertyCard } from "./PropertyCard";
