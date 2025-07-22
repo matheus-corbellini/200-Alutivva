0c784c0699be561e4a0e9881011cf164e47f3448
Matheus Corbellini <matheussteindorff@gmail.com>
2025-07-22 13:40:56 -0300

    feat: ajustes do bug estranho

---
+++ src/components/marketplace/index.ts
@@ -0,0 +1,8 @@
+export { Badge } from "./Badge";
+export { Header } from "./Header";
+export { HeroSection } from "./HeroSection";
+export { FiltersSection } from "./FiltersSection";
+export { ResultsSummary } from "./ResultsSummary";
+export { PropertiesGrid } from "./PropertiesGrid";
+export { EmptyState } from "./EmptyState";
+export { PropertyCard } from "./PropertyCard";
